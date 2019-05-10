bf403d90c963847d8a7072112bd7320c60264480
fanmengyuan <fanmengyuan@renrenche.com>
2019-05-10 16:38:00 +0800

    feat: 连接数据库

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # my-blog
 
+## 开发中的参考资料
+
+* [一步一步完成nodejs+express+mysql的简单实例开发](https://blog.csdn.net/hust_cxl/article/details/79929093)
+
+## 问题
+
+1. yarn serve 启动时遇到 "vue-cli-service 不是内部或外部命令,也不是可运行的程序"报错
+解决办法将项目里的“node_modules”文件夹删除，然后重新运行yarn install
+
 ## Project setup
 ```
 yarn install
---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');   //引入express模块
+var mysql = require('mysql');     //引入mysql模块
+var app = express();        //创建express的实例
+ 
+var connection = mysql.createConnection({      //创建mysql实例
+    host: 'localhost',
+    user: 'root',
+    password : 'root',
+    database:'sys'
+});
+connection.connect();
+var sql = 'SELECT * FROM message';
+var str = ""
+connection.query(sql, function (err,result) {
+    if(err){
+        console.log('[SELECT ERROR]:',err.message);
+    }
+    str = result
+    console.log(result);  //数据库查询结果返回到result中
+ 
+});
+app.get('/message',function (req,res) {
+    res.send(str);  ////服务器响应请求
+});
+
+var sql1 = 'SELECT * FROM info';
+var str1 = ""
+connection.query(sql1, function (err,result) {
+    if(err){
+        console.log('[SELECT ERROR]:',err.message);
+    }
+    str1 = result
+    console.log(result);  //数据库查询结果返回到result中
+ 
+});
+app.get('/info',function (req,res) {
+    res.send(str1);  ////服务器响应请求
+});
+
+
+connection.end();
+app.listen(3000,function () {    ////监听3000端口
+    console.log('Server running at 3000 port');
+});
